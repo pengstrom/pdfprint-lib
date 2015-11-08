4e779fcce674ccc9ceba264d3ef6a69c61b10e81
Per Engström <per.olov.engstrom@gmail.com>
2015-11-08 14:22:54 +0100

    Refactored module from pengstrom/pdfprint
    Split the logic from pengstrom/pdfprint into a separate library,
    pdfprint-lib.
    
    Uploading to webserver and remote ssh is finished.
    Printing is still in testing.

---
+++ src/PrintSSH.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace PEngstrom\PdfPrintLib;
+
+require 'Net/SSH2.php';
+require 'Net/SFTP.php';
+require 'Crypt/RSA.php';
+
+/**
+ * PrintSSH
+ *
+ * Uploads and prints files via SSH
+ */
+class PrintSSH
+{
+    /**
+     * SSH client
+     *
+     * @var Net_SSH2
+     */
+    protected $ssh;
+
+    /*
+     * SFTP client
+     *
+     * @var Net_SFTP
+     */
+    protected $sftp;
+
+    /**
+     * RSA private key
+     *
+     * @var Crypt_RSA
+     */
+    protected $key;
+
+    /**
+     * Initiates the ssh and sftp clients
+     *
+     * @param string $server   Server adress e.g. 'www.example.com'
+     * @param string $username User name
+     * @param string $keyfile  Location of private rsa key
+     */
+    public function __construct($server,
+                                $username,
+                                $keyfile) {
+        
+        $ssh = Net_SSH2($server);
+        $sftp = Net_SFTP($server);
+        $key = Crypt_RSA();
+
+        $key->loadKey(file_get_contents($keyfile));
+
+        if (!$ssh->login($username, $key) || !$ssh->login($username, $key)) {
+            exit('Access denied');
+        }
+
+        $this->sftp = $sftp;
+        $this->key = $key;
+    }
+    
+    /**
+     * Upload File
+     *
+     * Uploads file with the SFTp client
+     *
+     * @param string $file File to be uploaded
+     *
+     * @return void
+     */
+    public function uploadFile($file) {
+        $this->sftp->put($file, $file, NET_SFTP_LOCAL_FILE);
+        $this->sftp->chmod(0644, $file);
+    }
+
+    /**
+     * Print file
+     *
+     * Prints file on server with options
+     *
+     * @param string $file    File on server
+     * @param array  $options List of printer options
+     *
+     * @return void
+     */
+    public function printFile($file, $options) {
+
+        $this->uploadFile($file);
+
+        $printCommand = 'lpr -p 2402';
+        $command = $printCommand . ' ' . $file;
+
+        //$this->ssh->exec($command);
+    }
+}
---
+++ src/UploadHandler.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace PEngstorm\PdfPrintLib;
+
+require '../vendor/autoload.php';
+
+use Sirius\Validation\ValueValidation;
+use Sirius\Upload\Handler;
+
+/**
+ * Uploads and verifies pdf files
+ */
+class PdfUploadHandler extends Handler
+{
+
+    /**
+     * Calls Sirius\Upload\Handler and adds extension and mime rules
+     *
+     * @param mixed          $directoryOrContainer Local path for uploads
+     * @param array          $options              Options
+     * @param ValueValidator $validator            Validator 
+     */
+    public function __construct($directoryOrContainer,
+                                $options = array(),
+                                ValueValidator $validator = null) {
+
+        Parent::Constructor($directoryOrContainer,
+                            $options,
+                            $validator);
+
+        $this->addRule('extension',
+                       ['allowed' => ['pdf']],
+                       '{label} should be a valid pdf file.',
+                       'PDF file');
+
+        $this->addRule('callback',
+                       ['callback' => '$this->ensurePdfMime'],
+                       '{label} should be a valid PDF file.',
+                       'PDF file');
+    }
+     
+    /**
+     * Ensure the mime type of the file is pdf
+     *
+     * @param string $file File to be checked
+     *
+     * @return bool True of the check succeeds
+     */
+    public function ensurePdfMime($file) {
+        return finfo_file($file) === 'application/pdf';
+    }
+}
